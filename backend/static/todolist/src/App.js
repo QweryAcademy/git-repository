a444e0b0e2bf2662e8afa487fbb67c93243dcba7
biola <gbozee@gmail.com>
2017-03-19 17:02:04 +0100

    updated javascript code

--- backend/static/todolist/src/App.js
+++ backend/static/todolist/src/App.js
@@ -1,72 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
+import "./App.css";
 
-const Header = ({text}) => {
-  return (
-    <header className="header">
-      <h1>{text}</h1>
-      <input className="new-todo" placeholder="What needs to be done?" autofocus="" />
-    </header>)
-}
-const TodoList = () => {
-  return (
-    <section className="main block">
-      <input className="toggle-all" id="toggle-all" type="checkbox" />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul className="todo-list"><li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>jams</label>
-          <button className="destroy"></button>
-        </div>
-        <input className="edit" value="jams" />
-      </li><li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>shola</label>
-            <button className="destroy"></button>
-          </div>
-          <input className="edit" value="shola" />
-        </li><li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>shope</label>
-            <button className="destroy"></button>
-          </div>
-          <input className="edit" value="shope" />
-        </li></ul>
-    </section>
-  )
-}
-const Footer = () => {
-  return (
-    <footer className="footer block" >
-      <span className="todo-count"><strong>3</strong> items left</span>
-      <ul className="filters">
-        <li>
-          <a className="selected" href="#/">All</a>
-        </li>
-        <li>
-          <a href="#/active">Active</a>
-        </li>
-        <li>
-          <a href="#/completed">Completed</a>
-        </li>
-      </ul>
-
-    </footer>
-
-  )
-}
-
-function App() {
-  return (
-    <section className="todoapp">
-      <Header text={"Our todos"} />
-      <TodoList />
-      <Footer />
-    </section>
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+    var that = this;
+    this.addTodo = text => {
+      let oldTodos = this.state.todos;
+      // var response = fetch("/add_todo/", {
+      //   method: "post",
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify({ content: "hello world" })
+      // });
+      // response
+      //   .then(function(data) {
+      //     return data.json();
+      //   })
+      //   .then(data => {
+      //     that.setState({ todos: data });
+      //   });
+      oldTodos.push({ id: 1, content: text, completed: false });
+      this.setState({ todos: oldTodos });
+      // this.setState(function(prevState, props){
+      //   prevState.todos = oldTodos
+      //   return prevState;
+      // })
+    };
+  }
+  componentDidMount() {
+    this.setState({ todos: this.props.todos || [] });
+    // this.setState(function(prevState, props) {
+    //   // return {...prevState, ...props}
+    //   prevState.todos = props.todos || [];
+    //   return prevState;
+    // });
+  }
+  render() {
+    return (
+      <section className="todoapp">
+        <Header addTodo={this.addTodo} text={"Our todos"} />
+        <section className="main block">
+          <input className="toggle-all" id="toggle-all" type="checkbox" />
+          <label for="toggle-all">Mark all as complete</label>
+          <TodoList items={this.state.todos} />
+        </section>
+        <Footer />
+      </section>
+    );
+  }
 }
 
 export default App;
--- backend/static/todolist/src/index.js
+++ backend/static/todolist/src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import John from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-
-ReactDOM.render(
-  <John />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App todos={window.all_todos} />, document.getElementById("root"));
